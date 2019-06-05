1dab83049cb8c837b0f991e1c5d978e2ba6886d0
Kamlesh Prajapat <kamlesh.071198@gmail.com>
2019-06-05 20:41:48 +0530

    Made Responsive

--- js/script.js
+++ js/script.js
@@ -1,15 +1,49 @@
-const card = document.querySelectorAll('.main-card');
-var timeout;
-card.forEach(element =>  {
-    console.log(element)
-});
-function fliptest(){
-    var x = card;
-    x[0].style.transform = "rotateY(180deg)";
-    console.log(typeof card);
-    timeout = setTimeout(reFlip,3000);
+const cards = document.querySelectorAll('.main-card');
+let hasflippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipcard(){
+    this.classList.toggle('flip'); 
+
+    if(!hasflippedCard){
+        //first card clicked
+        hasflippedCard = true;
+        firstCard = this;
+    }else{
+        //second card clicked
+        hasflippedCard = false;
+        secondCard = this;
+     
+        matchCard();  
+    }
 }
-function reFlip(){
-    var x = card;
-    x[0].style.transform = "rotateY(0deg)";
+ 
+function matchCard(){  
+    //Cards Matched
+    if(firstCard.dataset.img === secondCard.dataset.img){
+        removeFlip();
+    }else{
+    //Cards not Matched
+        addFlip();
+    }
 }
+
+function removeFlip(){
+    firstCard.removeEventListener('click',flipcard);
+    secondCard.removeEventListener('click',flipcard);
+}
+
+function addFlip(){
+    lockBoard = true;
+
+    setTimeout(() =>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockBoard = false;
+        },1500);
+}
+
+
+cards.forEach(card => card.addEventListener('click',flipcard));
